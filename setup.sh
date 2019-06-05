c0793a7117a909449ae584c9d3fb816e8152b7c3
Adam Tew <adamjtew@gmail.com>
2019-06-04 23:43:05 -0600

    Added .gitignore for global gitignore

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 # brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew bundle # in this folder
 
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -7,13 +8,14 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 # Use fantasque-sans font in iterm2
 # https://github.com/belluzj/fantasque-sans#installation
 
-# material-design-colors in iterm
-
-# tmux plugin manager
-# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# colors for iterm: material-design-colors
 
 # Source all the dotfiles in the repo to the local version
 ln -s -f ~/git/adamtew/dotfiles/.vimrc ~/.vimrc
 ln -s -f ~/git/adamtew/dotfiles/.zshrc ~/.zshrc
 ln -s -f ~/git/adamtew/dotfiles/.tmux.conf ~/.tmux.conf
 
+
+# Setting up git
+ln -s -f ~/git/adamtew/dotfiles/.gitignore ~/.gitignore
+git config --global core.excludesfile ~/.gitignore
