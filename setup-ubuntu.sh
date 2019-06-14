7d1ea4fa81c2c11897eda25237e3269381865bc5
Adam Tew <adamjtew@gmail.com>
2019-06-14 14:28:28 -0600

    Added some initial ubuntu setup

---
+++ setup-ubuntu.sh
@@ -0,0 +1,13 @@
+sudo apt-get update -y
+sudo apt-get install -y git
+sudo apt-get install -y xterm
+sudo apt-get install -y build-essential
+sudo apt-get install -y vim
+sudo apt install -y copyq
+sudo apt install -y curl
+
+# Source all the dotfiles in the repo to the local version
+# Make sure to use absolute paths instead of relative ones
+ln -s -f ~/git/adamtew/dotfiles/.vimrc ~/.vimrc
+ln -s -f ~/git/adamtew/dotfiles/.zshrc ~/.zshrc
+ln -s -f ~/git/adamtew/dotfiles/.tmux.conf ~/.tmux.conf
--- setup.sh
+++ setup.sh
@@ -14,14 +14,26 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 # colors for iterm: material-design-colors
 
 # Source all the dotfiles in the repo to the local version
+# Make sure to use absolute paths instead of relative ones
 ln -s -f ~/git/adamtew/dotfiles/.vimrc ~/.vimrc
 ln -s -f ~/git/adamtew/dotfiles/.zshrc ~/.zshrc
 ln -s -f ~/git/adamtew/dotfiles/.tmux.conf ~/.tmux.conf
 
 
 # Setting up git
+git confi --global user.name "name"
+git confi --global user.email "email"
+
+#global ignore
 ln -s -f ~/git/adamtew/dotfiles/.gitignore ~/.gitignore
 git config --global core.excludesfile ~/.gitignore
+
+# ssh keys
+ssh-keygen -t rsa -b 4096 -C "email"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+# Copy the contents of the public key to github or gitlab
+cat ~/.ssh/id_rsa.pub | xclip -i -selection clipboard
 
 # Setting up Heroku
 heroku login
